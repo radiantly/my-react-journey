58a4715425c9a23ed8280f6314f64a231c001241
Radiantly <44368997+radiantly@users.noreply.github.com>
2020-03-15 16:47:50 +0530

    Finished the entry section on jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,13 +2,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+  return 'Click on me!';
+}
 // Create a react component
 const App = function() {
-    return <div>Hi there!</div>;
+  return (
+    <div>
+      <label htmlFor="name" className="label">Enter name:</label>
+      <input type="text" id="name" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>{getButtonText()}</button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
 ReactDOM.render(
-    <App />,
-    document.getElementById("root")
+  <App />,
+  document.getElementById("root")
 );
