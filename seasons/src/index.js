10ef8bc8ad309fdcb9b9c3dfe9bf90df0b71dd95
Radiantly <44368997+radiantly@users.noreply.github.com>
2020-03-21 19:56:21 +0530

    Using state to show updated values to the user

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const App = () => {
-//   window.navigator.geolocation.getCurrentPosition(
-//     position => console.log(position),
-//     error => console.log(`Error: ${error}`) 
-//   );
-//   return <div>Hi there!</div>;
-// };
-
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      error => console.log(`Error: ${error}`) 
-    );
+  constructor(props) {
+    super(props);
 
-    return <div>Latitude1:</div>;
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+    if(this.state.lat)
+      return <div>Latitude: {this.state.lat}</div>;
+    else if(this.state.errorMessage)
+      return <div>Error: {this.state.errorMessage}</div>;
+    
+    return <div>Loading...</div>;
+    // return (
+    //   <div>
+    //     Latitude: {this.state.lat}
+    //     <br />
+    //     Error: {this.state.errorMessage}
+    //   </div>
+    // );
   }
 }
 
